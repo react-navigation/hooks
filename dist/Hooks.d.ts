0712009b3ed2546a7847cea63206248aa985673d
bySabi Files <bysabi.files@gmail.com>
2018-11-28 19:32:11 +0100

    Added some files that were left behind

---
+++ dist/Hooks.d.ts
@@ -0,0 +1,11 @@
+export declare function useNavigation(): {};
+export declare function useNavigationParam(paramName: any): any;
+export declare function useNavigationState(): any;
+export declare function useNavigationKey(): any;
+export declare function useNavigationEvents(handleEvt: any): void;
+export declare function useFocusState(): {
+    isFocused: boolean;
+    isBlurring: boolean;
+    isBlurred: boolean;
+    isFocusing: boolean;
+};
