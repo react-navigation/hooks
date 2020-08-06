723b8acf35fa241471f3630a882c151aff183395
teivienn <teivienn@gmail.com>
2020-08-07 02:38:51 +0300

    fix: useNavigation generic

--- src/Hooks.ts
+++ src/Hooks.ts
@@ -17,13 +17,21 @@ import {
   EventType,
 } from 'react-navigation';
 
-export function useNavigation<S>(): NavigationScreenProp<S & NavigationRoute> {
-  const navigation = useContext(NavigationContext) as any; // TODO typing?
+type NavigationType<S> = S & NavigationScreenProp<NavigationRoute>;
+
+type NavigationWithParamType<S, P> = NavigationType<S> & NavigationScreenProp<NavigationRoute, P>;
+
+export function useNavigation<S>(): NavigationType<S>;
+
+export function useNavigation<S, P>(): NavigationWithParamType<S, P>;
+
+export function useNavigation() {
+  const navigation = useContext(NavigationContext);
   if (!navigation) {
     throw new Error(
       "react-navigation hooks require a navigation context but it couldn't be found. " +
-        "Make sure you didn't forget to create and render the react-navigation app container. " +
-        'If you need to access an optional navigation object, you can useContext(NavigationContext), which may return'
+      "Make sure you didn't forget to create and render the react-navigation app container. " +
+      'If you need to access an optional navigation object, you can useContext(NavigationContext), which may return'
     );
   }
   return navigation;
